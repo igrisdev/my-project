814356c51ac47762b13823e8129faa458ac15124
igrisdev <johanalvarezz237@gmail.com>
2025-04-08 13:45:51 -0500

    feat: implement pagination handling in ProductsStateService with changePage and loadProducts streams

--- src/app/products/data-access/products-state.services.ts
+++ src/app/products/data-access/products-state.services.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
 import { signalSlice } from 'ngxtension/signal-slice';
 import { ProductsService } from './products.service';
-import { map } from 'rxjs';
+import { catchError, map, of, startWith, Subject, switchMap } from 'rxjs';
 
 interface State {
   products: Product[];
@@ -20,12 +20,27 @@ export class ProductsStateService {
     page: 1,
   };
 
+  changePage$ = new Subject<number>();
+
+  loadProducts$ = this.changePage$.pipe(
+    startWith(1),
+    switchMap((page) => this.productsService.getProducts(page)),
+    map((products) => ({ products, status: 'success' as const })),
+    catchError(() => {
+      return of({
+        products: [],
+        status: 'error' as const,
+      });
+    })
+  );
+
   state = signalSlice({
     initialState: this.initialState,
     sources: [
-      this.productsService
-        .getProducts(1)
-        .pipe(map((products) => ({ products, status: 'success' as const }))),
+      this.changePage$.pipe(
+        map((page) => ({ page, status: 'loading' as const }))
+      ),
+      this.loadProducts$,
     ],
   });
 }
