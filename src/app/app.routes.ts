7644a3761e0fa26f95d383ebe198ef6e320aca97
igrisdev <johanalvarezz237@gmail.com>
2025-04-08 18:06:07 -0500

    feat(cart): add routing for cart module and initialize cart routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ export const routes: Routes = [
       import('./products/features/product-shell/product.route'),
   },
   {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.routes'),
+  },
+  {
     path: '**',
     redirectTo: 'products',
   },
