518ec3f5f540334375014b3ecc48527d2f153db7
igrisdev <johanalvarezz237@gmail.com>
2025-04-07 19:12:43 -0500

    feat: refactor ProductsService to extend BaseHttpService and use dynamic API URL

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -1,11 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { BaseHttpService } from '../../shared/data-access/base-http.service';
 
 @Injectable()
-export class ProductsService {
-  private http = inject(HttpClient);
-
+export class ProductsService extends BaseHttpService {
   getProducts() {
-    return this.http.get('https://fakestoreapi.com/products');
+    return this.http.get(`${this.apiUrl}/products`);
   }
 }
