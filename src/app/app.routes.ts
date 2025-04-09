2baea21e3b94e92d91a17667d7c44743007c6230
igrisdev <johanalvarezz237@gmail.com>
2025-04-08 19:57:52 -0500

    fix(routes): correct redirect configuration for default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     loadChildren: () =>
       import('./products/features/product-shell/product.route'),
   },
